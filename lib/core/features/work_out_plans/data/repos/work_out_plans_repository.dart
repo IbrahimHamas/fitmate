d1838a0cf8d43bef153d01ed196c2e91c4fa1633
MohamedHany1512 <mh861590@gmail.com>
2026-09-21 14:29:55 +0300

    moving getting data to remote data source

--- lib/core/features/work_out_plans/data/repos/work_out_plans_repository.dart
+++ lib/core/features/work_out_plans/data/repos/work_out_plans_repository.dart
@@ -2,20 +2,14 @@ import 'package:dartz/dartz.dart';
 import 'package:fitmate/core/features/work_out_plans/data/models/work_out_plans_model.dart';
 import 'package:fitmate/core/services/errors/failure.dart';
 import 'package:fitmate/core/services/request_handler.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../data_sources/work_out_plans_remote_data_sources.dart';
 
 class WorkoutPlansRepository {
-  final SupabaseClient _supabaseClient;
+  final WorkoutPlansRemoteDataSource _remoteDataSource;
 
-  WorkoutPlansRepository(this._supabaseClient);
+  WorkoutPlansRepository(this._remoteDataSource);
 
-  Future<Either<Failure, List<WorkoutPlanModel>>> getWorkoutPlans() async {
-    return requestHandler(() async {
-      final response = await _supabaseClient.from('workout_plans').select();
-
-      return (response as List)
-          .map((e) => WorkoutPlanModel.fromJson(e))
-          .toList();
-    });
-  }
+  Future<Either<Failure, List<WorkoutPlanModel>>> getWorkoutPlans() =>
+      requestHandler(_remoteDataSource.getWorkoutPlans);
 }
